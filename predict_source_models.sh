e8c9987811517347040bcb2de16f9cbe6c6526db
Jbar-ry <james.barry26@mail.dcu.ie>
2019-07-09 18:58:18 +0100

    automatically predict using source models

---
+++ predict_source_models.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+for lang in dan swe nno nob; do
+  # assign tbid to language
+  if [ "${lang}" = "dan" ]; then
+    tbid=da_ddt
+    echo "projecting source files from $tbid"
+  elif [ "${lang}" = "swe" ]; then
+    tbid=sv_talbanken
+    echo "projecting source files from $tbid"
+  elif [ "${lang}" = "nno" ]; then
+    tbid=no_nynorsk
+    echo "projecting source files from $tbid"
+  elif [ "${lang}" = "nob" ]; then
+    tbid=no_bokmaal
+    echo "projecting source files from $tbid"
+  fi
+  
+  # path to source udpipe tagged/tokenized file to predict
+  PRED_FILE=data/faroese/fao_wiki.apertium.fao-${lang}.udpipe.parsed.conllu  
+  
+  # file to write
+  OUT_FILE=output/monolingual/predicted/fao_wiki.apertium.fao-${tbid}.allennlp.parsed.conllu
+  
+  allennlp predict output/monolingual/source_models/${tbid}/model.tar.gz ${PRED_FILE} \
+      --output-file ${OUT_FILE} \
+      --predictor biaffine-dependency-parser-monolingual \
+      --include-package library \
+      --use-dataset-reader
+
+
