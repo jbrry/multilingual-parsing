eb0c89f7ce36c8e33ad8f6b36710499fdba4cabf
Jbar-ry <james.barry26@mail.dcu.ie>
2019-07-11 13:25:07 +0100

    fix eval variables

--- predict_monolingual_target_parser.sh
+++ predict_monolingual_target_parser.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-#for lang in dan swe nno nob; do
-for lang in nno; do
+for lang in dan swe nno nob; do
   OUTFILE=output/monolingual/target_predicted/${lang}-fao.conllu
   TEST_FILE=/home/jbarry/ud-parsing/ud-treebanks-v2.2/UD_Faroese-OFT/fo_oft-ud-test.conllu
   
@@ -16,6 +15,6 @@ for lang in nno; do
     --include-package library \
     --use-dataset-reader
  
-  echo "running evaluation script with gold file ${TEST_FILE} and pred file ${OUTFILE}"
-  python utils/conll18_ud_eval.py --verbose /home/jbarry/ud-parsing/ud-treebanks-v2.2/UD_Faroese-OFT/fo_oft-ud-test.conllu output/monolingual/target_predicted/${lang}-fao.conllu > output/monolingual/results/${lang}-fao.result
+  echo "running evaluation script with gold file ${TEST_FILE} and output file ${OUTFILE}"
+  python utils/conll18_ud_eval.py --verbose ${TEST_FILE} ${OUTFILE} > output/monolingual/results/${lang}-fao.result
 done
