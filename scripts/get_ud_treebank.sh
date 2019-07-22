e22b6dddce04cd0a040bfcec74a89bf2dd37a6b1
Jbar-ry <james.barry26@mail.dcu.ie>
2019-07-22 18:22:45 +0100

    download ud data to data folder

--- scripts/get_ud_treebank.sh
+++ scripts/get_ud_treebank.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# move to the directory you want to save the treebanks ('ud-parsing')
-cd && cd ud-parsing
+# change directory to data dir
+cd data
 
 # get the UD 2.2 treebanks/tools
 curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-2837{/ud-treebanks-v2.2.tgz}
