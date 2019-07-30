6954b73796f633e168e335946112e6261945afa9
Jbar-ry <james.barry26@mail.dcu.ie>
2019-07-30 18:31:36 +0100

    update cross val scripts

--- scripts/create_k_folds.sh
+++ scripts/create_k_folds.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 TB_DIR='data/ud-treebanks-v2.2'
+TMP_DIR='data/tmp'
 OUT_DIR='data/ud-treebanks-v2.2-crossfold-tags'
 
-mkdir -p ${OUT_DIR}
+mkdir -p ${TMP_DIR} ${TMP_DIR} ${OUT_DIR}
 
 TIMESTAMP=`date "+%Y%m%d-%H%M%S"` 
 
 for tbid in da_ddt sv_talbanken no_nynorsk no_bokmaal; do
   for filepath in $TB_DIR/*/$tbid-ud-train.conllu; do 
   
-    dir=`dirname $filepath`        # e.g. /home/james/ud_folder/ud-treebanks-v2.2/UD_Afrikaans-AfriBooms
-    tb_name=`basename $dir`        # e.g. UD_Afrikaans-AfriBooms
+    dir=`dirname $filepath`
+    tb_name=`basename $dir`
 
-    #ln -s ${TB_DIR}/${tb_name}/${tbid}-ud-train.conllu ${OUT_DIR}/${tb_name}/
- 
-    python utils/create_k_folds.py -t ${TB_DIR}/${tb_name}/${tbid}-ud-train.conllu -o ${OUT_DIR}/${tb_name}/ 
+    # we only do jack-knifing for the training sets, see: http://universaldependencies.org/conll18/baseline.html
+    python utils/create_k_folds.py -i ${TB_DIR}/${tb_name}/${tbid}-ud-train.conllu -o ${TMP_DIR}/${tb_name}/
+
   done
 done
 
--- utils/create_k_folds.py
+++ utils/create_k_folds.py
@@ -7,7 +7,7 @@ import sys, os, re, copy
 import codecs
 
 parser = argparse.ArgumentParser(description='Cross Validation Arguments')
-parser.add_argument('--train', '-t', type=str, help='Training file to split into chunks.')
+parser.add_argument('--input', '-i', type=str, help='Training file to split into chunks.')
 parser.add_argument('--outdir','-o', type=str, help='Directory to write split files to.')
 parser.add_argument('--k', '-k', type=int, default=10, help='Number of folds for cross-validation.')
 parser.add_argument('--encoding', '-e', type=str, default='utf-8', help='Type of encoding.')
@@ -19,17 +19,17 @@ args = parser.parse_args()
 if not os.path.exists(args.outdir):
     os.mkdir(args.outdir)
 
-train_name = os.path.basename(args.train)
-tbid = train_name.split('-')[0]
+in_name = os.path.basename(args.input)
+tbid = in_name.split('-')[0]
 
 
-def conllu_reader(trainfile):
+def conllu_reader(infile):
     """Simple CoNLL-U reader to return a list of sentences from a file 
        as well as token/sentence counts."""
     
-    print("Reading sentences from {}".format(trainfile))
+    print("Reading sentences from {}".format(infile))
 
-    file = codecs.open(trainfile, 'r', encoding=args.encoding)
+    file = codecs.open(infile, 'r', encoding=args.encoding)
     
     sentences = []
     current_sentence = []
@@ -67,11 +67,11 @@ def conllu_reader(trainfile):
     return sentences, token_counts, sent_count
 
 
-def generate_k_folds(train_sentences, k):
-    """Divides training sentences into k folds.
+def generate_k_folds(in_sentences, k):
+    """Divides input sentences into k folds.
     
     Args:
-        train_sentences: list of lists containing conllu sentences.
+        in_sentences: list of lists containing conllu sentences.
         k: number of folds to create (default=10).
         
     Returns:
@@ -80,9 +80,9 @@ def generate_k_folds(train_sentences, k):
     # main list which will contain the folds
     data_folds = []
     # keep a copy of the dataset
-    dataset_copy = list(train_sentences)  
-    sents_per_fold = int(len(train_sentences) / k)
-    remainder = int(len(train_sentences) % k)
+    dataset_copy = list(in_sentences)  
+    sents_per_fold = int(len(in_sentences) / k)
+    remainder = int(len(in_sentences) % k)
 
     required_sizes = remainder * [sents_per_fold + 1] + (k - remainder) * [sents_per_fold]
     print("Splitting the dataset into {} folds containing {} sentences \n".format(args.k, required_sizes))
@@ -139,11 +139,11 @@ def write_conllu(data, outfile):
                 f.write('\n')
 
 
-# create training sentences and token/sentence counts
-train_sentences, token_counts, sent_count = conllu_reader(args.train)
+# create input sentences and token/sentence counts
+in_sentences, token_counts, sent_count = conllu_reader(args.input)
 
-# turn training dataset into k folds
-folded_dataset = generate_k_folds(train_sentences, args.k)
+# turn input sentences into k folds
+folded_dataset = generate_k_folds(in_sentences, args.k)
 assert len(folded_dataset) == args.k
 
 # create train/dev splits
@@ -151,9 +151,8 @@ for i, fold in enumerate(folded_dataset):
     train_split, dev_split = make_train_dev_splits(fold, folded_dataset)
     print("Creating split %d" % (i))
     assert len(train_split) == args.k - 1
-    print()
     
-    conllu_train = train_name + '.' + 'split-'+ str(i)
+    conllu_train = tbid + '-ud-train.conllu' + '.' + 'split-'+ str(i)
     conllu_dev = tbid + '-ud-dev.conllu' + '.' + 'split-' + str(i)
     
     train_out = os.path.join(args.outdir, conllu_train)  
@@ -162,4 +161,4 @@ for i, fold in enumerate(folded_dataset):
     train_sample = write_conllu(train_split, train_out)
     dev_sample = write_conllu(dev_split, dev_out)
 
-print("Finished. Wrote files to {}".format(args.outdir))
+print("Finished. Wrote files to {} \n".format(args.outdir))
