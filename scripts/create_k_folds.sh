376ac53ffc83b76e8a30caac4baa36805c6b83b2
Jbar-ry <james.barry26@mail.dcu.ie>
2019-07-30 16:08:41 +0100

    update outdir

--- scripts/create_k_folds.sh
+++ scripts/create_k_folds.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 TB_DIR='data/ud-treebanks-v2.2'
+OUT_DIR='data/ud-treebanks-v2.2-crossfold-tags'
+
+mkdir -p ${OUT_DIR}
+
 TIMESTAMP=`date "+%Y%m%d-%H%M%S"` 
 
 for tbid in da_ddt sv_talbanken no_nynorsk no_bokmaal; do
@@ -8,8 +12,10 @@ for tbid in da_ddt sv_talbanken no_nynorsk no_bokmaal; do
   
     dir=`dirname $filepath`        # e.g. /home/james/ud_folder/ud-treebanks-v2.2/UD_Afrikaans-AfriBooms
     tb_name=`basename $dir`        # e.g. UD_Afrikaans-AfriBooms
+
+    #ln -s ${TB_DIR}/${tb_name}/${tbid}-ud-train.conllu ${OUT_DIR}/${tb_name}/
  
-    python utils/create_k_folds.py -t ${TB_DIR}/${tb_name}/${tbid}-ud-train.conllu -o ${TB_DIR}/${tb_name}/ 
+    python utils/create_k_folds.py -t ${TB_DIR}/${tb_name}/${tbid}-ud-train.conllu -o ${OUT_DIR}/${tb_name}/ 
   done
 done
 
